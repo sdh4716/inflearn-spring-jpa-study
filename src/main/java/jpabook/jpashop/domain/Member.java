ce03c946a2ddc1d207e40d02a991c0a746e305c8
Yunable <dearyunable@gmail.com>
2025-07-05 00:38:44 +0900

    도메인 생성 및 연관관계 매핑 학습 완료

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter // 실무에서는 Setter는 꼭 필요할 때만 열어 줌
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
