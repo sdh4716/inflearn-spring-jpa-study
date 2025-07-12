3887b714eb481a2b3514bf4e58283e32d2b6b96a
darong <sdh4716@naver.com>
2025-07-12 23:02:52 +0900

    V5 컬렉션 조회 최적화 학습 완료

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -104,6 +104,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter // 또는 @Data -> 안쓰는게 나은 경우도 있음.
     static class OrderDto {
 
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -16,4 +16,12 @@ public class OrderQueryDto {
     private OrderStatus orderStatus;
     private Address address;
     private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
 }
