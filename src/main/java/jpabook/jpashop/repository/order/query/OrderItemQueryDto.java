5dcc94414bcb8fc19b6eb3e29bfbd5877885c3c1
dev1song <sdh4716@naver.com>
2025-07-12 16:14:33 +0900

    commit test

--- src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -1,4 +1,12 @@
 package jpabook.jpashop.repository.order.query;
 
+import lombok.Data;
+
+@Data
 public class OrderItemQueryDto {
+
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
 }
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -1,4 +1,22 @@
 package jpabook.jpashop.repository.order.query;
 
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class OrderQueryRepository {
+
+    private final EntityManager em;
+
+    public List<OrderQueryDto> findOrderQueryDtos() {
+        return em.createQuery(
+                "select from jpabook.jpashop.repository.query.OrderQueryDto(o.id, m.name, o.orderDate, o.orderState, d.address" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderQueryDto.class)
+                .getResultList();
+    }
 }
